ee9e56c2c4b8982b13db397d3339ca1ba04f729b
anjalig21 <anjaligta@outlook.com>
2021-12-05 21:38:07 -0500

    added dropdown to navbar

--- shrikant/src/components/navbar/navbar.jsx
+++ shrikant/src/components/navbar/navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import logo from './assets/shrilogo.png'
-import './navbar.css'
+import Container from "react-bootstrap/Container";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Nav from "react-bootstrap/Nav";
+import logo from "./assets/shrilogo.png";
+import "./navbar.css";
 
 const MyNavbar = () => {
   return (
@@ -15,7 +16,26 @@ const MyNavbar = () => {
           </Navbar.Brand>
           <Nav className="justify-content-end flex-grow-1 pe-3">
             <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="insurance">Insurance</Nav.Link>
+            <NavDropdown title="Insurance" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#life-insurance">
+                Life Insurance
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#critical-illness">
+                Critical Illness Insurance
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#disability-insurance">
+                Disability Insurance
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#mortgage-insurance">
+                Mortgage Insurance
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#druganddental-insurance">
+                Drug and Dental Insurance
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#nomedicallife-insurance">
+                No Medical Life Insurance
+              </NavDropdown.Item>
+            </NavDropdown>
             <Nav.Link href="investments">Investments</Nav.Link>
             <Nav.Link href="supervisa">Supervisa</Nav.Link>
             <Nav.Link href="about">About</Nav.Link>
