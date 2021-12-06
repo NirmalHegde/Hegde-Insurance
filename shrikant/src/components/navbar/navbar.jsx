c303700b0667de761855584c3bbb3fad3ef9f491
Nirmal Hegde <nirmalhegde821@gmail.com>
2021-12-05 21:07:32 -0500

    navbar logo

--- shrikant/src/components/navbar/navbar.jsx
+++ shrikant/src/components/navbar/navbar.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import logo from './assets/shrilogo.png'
+import './navbar.css'
 
 const MyNavbar = () => {
   return (
     <>
       <Navbar sticky="top" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Shrikant</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img className="navlogo" src={logo} alt="shrilogo" />
+          </Navbar.Brand>
           <Nav className="justify-content-end flex-grow-1 pe-3">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#insurance">Insurance</Nav.Link>
