fc36486878860a597b06cbf52316210956bd03e8
Nirmal Hegde <nirmalhegde821@gmail.com>
2021-12-05 21:20:23 -0500

    contact page init

--- shrikant/src/components/navbar/navbar.jsx
+++ shrikant/src/components/navbar/navbar.jsx
@@ -14,12 +14,12 @@ const MyNavbar = () => {
             <img className="navlogo" src={logo} alt="shrilogo" />
           </Navbar.Brand>
           <Nav className="justify-content-end flex-grow-1 pe-3">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#insurance">Insurance</Nav.Link>
-            <Nav.Link href="#investments">Investments</Nav.Link>
-            <Nav.Link href="#supervisa">Supervisa</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="insurance">Insurance</Nav.Link>
+            <Nav.Link href="investments">Investments</Nav.Link>
+            <Nav.Link href="supervisa">Supervisa</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
