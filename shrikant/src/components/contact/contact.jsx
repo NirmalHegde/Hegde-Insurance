6116931ef5182f862ddbb2662edda4bc6edd0701
Nirmal Hegde <nirmalhegde821@gmail.com>
2021-12-06 20:22:11 -0500

    state management

--- shrikant/src/components/contact/contact.jsx
+++ shrikant/src/components/contact/contact.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 const Contact = () => {
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [subject, setSubject] = useState("");
+    const [body, setBody] = useState("");
+    const submitForm = (e) => {
+        console.log(email);
+        e.preventDefault();
+    }
+
     return (
-        <Form>
+        <Form onSubmit={submitForm}>
             <Form.Group className="mb-3" controlId="email">
                 <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" placeholder="name@example.com" />
+                <Form.Control value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="name@example.com" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="name">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="John Doe" />
+                <Form.Control value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="John Doe" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="subject">
                 <Form.Label>Subject</Form.Label>
-                <Form.Control type="text" placeholder="Life Insurance" />
+                <Form.Control value={subject} onChange={(e) => setSubject(e.target.value)} type="text" placeholder="Life Insurance" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="body">
                 <Form.Label>Message</Form.Label>
-                <Form.Control type="textarea" placeholder="Let's do business!" />
+                <Form.Control value={body} onChange={(e) => setBody(e.target.value)} type="textarea" placeholder="Let's do business!" />
             </Form.Group>
             <Button variant="primary" type="submit">
                 Submit
